1cd3a360c3f77d43b408d84e7201aa922054f4b5
wirda diana nesywa <92450175+WirdaDiana13@users.noreply.github.com>
2023-09-14 21:10:54 +0700

    Revert "myprofile"

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Controllers\Home;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-// $routes->get('/profile', 'Home::profile');
-$routes->get('/profile', [Home::class, 'profile']);
